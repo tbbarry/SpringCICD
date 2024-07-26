e2e7d5465fb4d1aed9768bf35987de96bef54a32
Thierno BARRY <tbbarry8@gmail.com>
2024-07-26 12:21:05 +0200

    Github workflow

--- src/test/java/com/training/justdab/JustdabApplicationTests.java
+++ src/test/java/com/training/justdab/JustdabApplicationTests.java
@@ -2,10 +2,26 @@ package com.training.justdab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class JustdabApplicationTests {
-
-
+	@Container
+	private static final MySQLContainer<?> SQL_CONTAINER = new MySQLContainer<>()
+			.withDatabaseName("just_dab_db");
+	@DynamicPropertySource
+	static void configureMySqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", SQL_CONTAINER::getJdbcUrl);
+		registry.add("spring.datasource.username", SQL_CONTAINER::getUsername);
+		registry.add("spring.datasource.password", SQL_CONTAINER::getPassword);
+	}
+	@Test
+	void contextLoads() {
+	}
 
 }
